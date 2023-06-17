8d63fd3583448d71865ef1f0dfa8159a8406dd06
wojdzie <wojdzi@outlook.com>
2023-06-17 22:45:02 +0200

    Refactored python_git.py

--- python_git.py
+++ python_git.py
@@ -7,32 +7,32 @@ import commit
 import init
 import log
 
+arg_parser = argparse.ArgumentParser()
+arg_subparsers = arg_parser.add_subparsers(title="Commands", dest="command")
+arg_subparsers.required = True
+
+# Init command
+arg_sp_init = arg_subparsers.add_parser("init", help="Initialize a new, empty repository.")
+arg_sp_init.add_argument("path", metavar="directory", nargs="?", default=".", help="Where to create the repository")
+
+# Add command
+arg_sp_add = arg_subparsers.add_parser("add", help="Add file(s) to the staging area.")
+arg_sp_add.add_argument("files", metavar="file", nargs="+", help="File(s) to add")
+
+# Commit command
+arg_sp_commit = arg_subparsers.add_parser("commit", help="Create a commit from staged changes")
+
+# Log command
+arg_sp_log = arg_subparsers.add_parser("log", help="Display commit history for the repository.")
+
+# Checkout command
+arg_sp_checkout = arg_subparsers.add_parser("checkout", help="Switch branches.")
+arg_sp_checkout.add_argument("branch", metavar="branch", help="Branch to switch to")
+
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
-
-    arg_parser = argparse.ArgumentParser()
-    arg_subparsers = arg_parser.add_subparsers(title="Commands", dest="command")
-    arg_subparsers.required = True
-
-    # Init command
-    arg_sp_init = arg_subparsers.add_parser("init", help="Initialize a new, empty repository.")
-    arg_sp_init.add_argument("path", metavar="directory", nargs="?", default=".", help="Where to create the repository")
-
-    # Add command
-    arg_sp_add = arg_subparsers.add_parser("add", help="Add file(s) to the staging area.")
-    arg_sp_add.add_argument("files", metavar="file", nargs="+", help="File(s) to add")
-
-    # Commit command
-    arg_sp_commit = arg_subparsers.add_parser("commit", help="Create a commit from staged changes")
-
-    # Log command
-    arg_sp_log = arg_subparsers.add_parser("log", help="Display commit history for the repository.")
-
-    # Checkout command
-    arg_sp_checkout = arg_subparsers.add_parser("checkout", help="Switch branches.")
-    arg_sp_checkout.add_argument("branch", metavar="branch", help="Branch to switch to")
 
     args = arg_parser.parse_args(argv)
 
