f3c4930234babae8bad7e8de21bc1ac0167f207d
wojdzie <wojdzi@outlook.com>
2023-06-17 02:13:15 +0200

    Implemented pygit init command

--- python_git.py
+++ python_git.py
@@ -1,117 +1,27 @@
-#!/usr/bin/env python3
-
-import argparse
-import configparser
-
 import os
-import sys
 
-arg_parser = argparse.ArgumentParser(description="The stupidest content tracker")
-arg_subparsers = arg_parser.add_subparsers(title="Commands", dest="command")
-arg_subparsers.required = True
-
-arg_sp = arg_subparsers.add_parser("init", help="Initialize a new, empty repository.")
-arg_sp.add_argument("path", metavar="directory", nargs="?", default=".", help="Where to create the repository")
+REPO_DIR = ".pygit"
 
 
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv[1:]
-    args = arg_parser.parse_args(argv)
+def init(args):
+    repo_path = os.path.abspath(args.path)
+    pygit_dir = os.path.join(repo_path, REPO_DIR)
 
-    if args.command == "init":
-        cmd_init(args)
+    if os.path.exists(pygit_dir):
+        print("Repository already exists.")
+        return
 
+    os.makedirs(pygit_dir, exist_ok=True)
+    print(f"Initialized empty repository in {pygit_dir}")
 
-def repo_path(repo, *path):
-    return os.path.join(repo.gitdir, *path)
+    staging_dir = os.path.join(pygit_dir, "staging")
+    os.makedirs(staging_dir, exist_ok=True)
 
+    commits_dir = os.path.join(pygit_dir, "commits")
+    os.makedirs(commits_dir, exist_ok=True)
 
-def repo_file(repo, *path, mkdir=False):
-    if repo_dir(repo, *path[:-1], mkdir=mkdir):
-        return repo_path(repo, *path)
+    head_file = os.path.join(pygit_dir, "HEAD")
+    with open(head_file, "w") as f:
+        f.write("master")
 
-
-def repo_dir(repo, *path, mkdir=False):
-    path = repo_path(repo, *path)
-
-    if os.path.exists(path):
-        if os.path.isdir(path):
-            return path
-        else:
-            raise Exception(f"Not a directory {path}")
-
-    if mkdir:
-        os.makedirs(path)
-        return path
-    else:
-        return None
-
-
-def repo_create(path):
-    repo = GitRepository(path, True)
-
-    if os.path.exists(repo.worktree):
-        if not os.path.isdir(repo.worktree):
-            raise Exception(f"{path} is not a directory!")
-        if os.listdir(repo.worktree):
-            raise Exception(f"{path} is not empty!")
-    else:
-        os.makedirs(repo.worktree)
-
-    assert(repo_dir(repo, "branches", mkdir=True))
-    assert(repo_dir(repo, "objects", mkdir=True))
-    assert(repo_dir(repo, "refs", "tags", mkdir=True))
-    assert(repo_dir(repo, "refs", "heads", mkdir=True))
-
-    with open(repo_file(repo, "description"), "w") as f:
-        f.write("Unnamed repository: edit this file 'description' to name the repository.\n")
-
-    with open(repo_file(repo, "HEAD"), "w") as f:
-        f.write("ref: refs/heads/master\n")
-
-    with open(repo_file(repo, "config"), "w") as f:
-        config = repo_default_config()
-        config.write(f)
-
-    return repo
-
-
-def repo_default_config():
-    ret = configparser.ConfigParser()
-    ret.add_section("core")
-    ret.set("core", "repositoryformatversion", "0")
-    ret.set("core", "filemode", "false")
-    ret.set("core", "bare", "false")
-
-    return ret
-
-
-def cmd_init(args):
-    repo_create(args.path)
-
-
-class GitRepository(object):
-    worktree = None
-    gitdir = None
-    conf = None
-
-    def __init__(self, path, force=False):
-        self.worktree = path
-        self.gitdir = os.path.join(path, ".git")
-
-        if not (force or os.path.isdir(self.gitdir)):
-            raise Exception(f"Not a Git repository {path}")
-
-        self.conf = configparser.ConfigParser()
-        cf = repo_file(self, "config")
-
-        if cf and os.path.exists(cf):
-            self.conf.read([cf])
-        elif not force:
-            raise Exception("Configuration file missing")
-
-        if not force:
-            vers = int(self.conf.get("core", "repositoryformatversion"))
-            if vers != 0:
-                raise Exception(f"Unsupported repositoryformatversion {vers}")
+    print(f"Initialized empty PyGit repository in {repo_path}/.pygit")
